c761a527cefdaad4574308184a92dc6fe0b68648
AmandaGValerio <valeriogamanda@gmail.com>
2021-10-24 16:00:08 -0300

    lendo a base de dados

--- scriptAnalise.R
+++ scriptAnalise.R
@@ -1,8 +1,15 @@
 #autores: Amanda, Laura
 
 #importação de bibliotecas necessárias
+install.packages("arules")
+install.packages("arulesSequences")
+
+
+library(arules)
+library(arulesSequences)
 
 #importação da base de dados
+empregados <- data.frame(read.csv(file="survey.csv", header=TRUE, sep=","))
 
 #tratamento dos dados
 
